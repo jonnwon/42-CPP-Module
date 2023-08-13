311ae39fb1ce9b8a3a0e9ebbf298e62cc4c6c4c8
Jongwon Choi <jonchoi@c4r1s4.42seoul.kr>
2023-08-13 16:43:43 +0900

    docs: CPP01/ex00 updates

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jonchoi <jonchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 19:03:18 by jonchoi           #+#    #+#             */
-/*   Updated: 2023/07/24 02:47:35 by jonchoi          ###   ########.fr       */
+/*   Updated: 2023/08/13 16:34:07 by jonchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 
 int main()
 {
+	/* stack test */
+	Zombie stack("Stack3");
+	stack.announce();
+
+	/* heap test */
 	Zombie *heap;
 
 	heap = newZombie("Heap");
 	heap->announce();
+	delete heap;
 
+	/* randomChump test */
 	randomChump("Stack1");
 
+	/* stackframe test*/
 	{
 		Zombie stack("Stack2");
 		stack.announce();
 	}
-	Zombie stack("Stack3");
-	delete heap;
-	stack.announce();
+
 	return (0);
 }
