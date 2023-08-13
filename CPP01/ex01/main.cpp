b2772e843950ec36e1e7ef6cef4d9e90670ca315
Jongwon Choi <jonchoi@c4r1s4.42seoul.kr>
2023-08-13 16:43:59 +0900

    docs: CPP01/ex01 updates

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -4,15 +4,18 @@ int main()
 {
 	Zombie	stack;
 	Zombie	*heap;
-	int		N = 3;
+	int		N;
 
-	stack.set_name("Stack");
+	std::cout << "Please enter the number of zombies: ";
+	std::cin >> N;
+
 	heap = zombieHorde(N, "Heap");
-	stack.announce();
 	for (int i = 0; i < N; i++)
 	{
 		heap[i].announce();
 	}
 	delete[] heap;
+
+	system("leaks Moar_brainz");	//leak test
 	return (0);
 }
