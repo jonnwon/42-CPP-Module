0fd6e6c280192d4915a5e7298f422e3ef129a436
Jongwon Choi <jonchoi@c8r2s14.42seoul.kr>
2023-09-26 14:04:00 +0900

    update: cpp08/ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -2,12 +2,24 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+
+class NotFoundException : public std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return ("Failed to find value");
+    }
+};
 
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
-//	if (it == container.end())
+	if (it == container.end())
+		throw NotFoundException();
 	return it;
 }
 
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -1,19 +1,91 @@
 #include "easyfind.hpp"
-#include <vector>
 
 int main()
 {
-  // º¤ÅÍ »ý¼º
-  std::vector<int> v;
+	std::cout << "=======vector test =======" << std::endl;
 
-  for (int i = 0; i < 5; i++)
-	  v.push_back(i);
+	std::vector<int> v;
 
-  std::vector<int>::iterator it = v.begin() + 1;
+	for (int i = 0; i < 5; i++)
+		v.push_back(i);
 
-  std::cout << *it;
+	try
+	{
+		std::vector<int>::iterator it = easyfind(v, 1);
+		std::cout << "value: " << *it << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
-  std::cout << *easyfind(v, 3);
+	std::cout << "-------------------------" << std::endl;
+
+	try
+	{
+		std::vector<int>::iterator it = easyfind(v, 10);
+		std::cout << "value: " << *it << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "=======list test =======" << std::endl;
+
+	std::list<int> l;
+	for (int i = 0; i < 5; i++)
+		l.push_back(i);
+
+	try
+	{
+		std::list<int>::iterator it2 = easyfind(l, 1);
+		std::cout << "value: " << *it2 << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "-------------------------" << std::endl;
+
+	try
+	{
+		std::list<int>::iterator it2 = easyfind(l, 10);
+		std::cout << "value: " << *it2 << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "=======deque test =======" << std::endl;
+	
+	std::deque<int> d;
+	for (int i = 0; i < 5; i++)
+		d.push_back(i);
+
+	try
+	{
+		std::deque<int>::iterator it3 = easyfind(d, 1);
+		std::cout << "value: " << *it3 << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "-------------------------" << std::endl;
+
+	try
+	{
+		std::deque<int>::iterator it3 = easyfind(d, 10);
+		std::cout << "value: " << *it3 << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	return 0;
 }
