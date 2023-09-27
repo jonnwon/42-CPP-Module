56d5aa44b11198897c7bf47afd2c05d9ee16325d
Jongwon Choi <jonchoi@c8r1s14.42seoul.kr>
2023-09-27 18:27:58 +0900

    update: CPP08/ex02

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,41 +1,76 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
-    MutantStack<int> s;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
 
-    s.push(1);
-    s.push(2);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 
-    for (MutantStack<int>::iterator i=s.begin(); i != s.end(); i++)
-    {
-        *i = *i + 1;    // no error
-        std::cout << *i << " ";
-    }
+	++it;
+	--it;
 
-    std::cout << std::endl;
+	std::cout << "=============" << std::endl;
+	std::cout << "Compare with list" << std::endl;
+	std::cout << "mstack size: " << mstack.size() << std::endl;
+	std::cout << "mstack contents" << std::endl;
 
-    for (MutantStack<int>::const_iterator i=s.begin(); i != s.end(); i++)
-    {
-        //*i = *i + 1;     // error
-        std::cout << *i << " ";
-    }
+	while (it != ite)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
 
-    std::cout << std::endl;
+	std::cout << std::endl << "-----------" << std::endl;
 
-    for (MutantStack<int>::reverse_iterator i=s.rbegin(); i != s.rend(); i++)
-    {
-        *i = *i + 1;    // no error
-        std::cout << *i << " ";
-    }
+	std::list<int> list;
 
-    std::cout << std::endl;
+	list.push_back(5);
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
 
-    for (MutantStack<int>::const_reverse_iterator i=s.rbegin(); i != s.rend(); i++)
-    {
-        //*i = *i + 1;    //error
-        std::cout << *i << " ";
-    }
+	std::cout << "list size: " << list.size() << std::endl;
+	std::cout << "list contents" << std::endl;
 
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); it++)
+		std::cout << *it << " ";
 
+	std::cout << std::endl << "=============" << std::endl;
+	std::cout << "Copy constructor test" << std::endl;
+	{
+		MutantStack<int> copy(mstack);
+
+		std::cout << "Print copy" << std::endl;
+
+		for (MutantStack<int>::iterator it = copy.begin(); it != copy.end(); it++)
+		{
+			std::cout << *it << " ";
+		}
+	}
+
+	std::cout << std::endl << "=============" << std::endl;
+	std::cout << "Copy assignment operator test" << std::endl;
+	{
+		MutantStack<int> copy;
+
+		copy = mstack;
+		for (MutantStack<int>::iterator it = copy.begin(); it != copy.end(); it++)
+		{
+			std::cout << *it << " ";
+		}
+	}
+
+	return 0;
 }
