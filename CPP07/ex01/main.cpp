a8304794ac051c467ffeb9ebdc8a1d4e33ffcd2c
Jongwon Choi <jonchoi@c5r8s6.42seoul.kr>
2023-10-12 04:18:02 +0900

    fix: Add test case

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
+	char arr_char[3] = {'a', 'b', 'c'};
+	iter(arr_char, sizeof(arr_char) / sizeof(char), print<char>);
+
+	std::cout << std::endl << "---------------" << std::endl;
+
 	int arr_int[3] = {1, 2, 3};
 	iter(arr_int, sizeof(arr_int) / sizeof(int), print<int>);
 
@@ -14,6 +19,11 @@ int main()
 	
 	double arr_double[3] = {1.111, 2.222, 3.333};
 	iter(arr_double, sizeof(arr_double) / sizeof(double), print);
+
+	std::cout << std::endl << "---------------" << std::endl;
+	
+	float arr_float[3] = {1.111f, 2.222f, 3.333f};
+	iter(arr_float, sizeof(arr_float) / sizeof(float), print<float>);
 
 	return 0;
 }
